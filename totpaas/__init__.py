1558a4a7606adc22039624aaa9337fb3709f18fe
Frederick Wagner <frederick@frederickwagner.net>
2021-10-05 22:31:14 -0400

    Remove vestigial code

--- totpaas/__init__.py
+++ totpaas/__init__.py
@@ -1,14 +1,1 @@
 __version__ = '0.1.0'
-import json
-import os
-
-from fastapi import FastAPI
-
-from .main import TotpResource
-
-
-params = json.loads(os.environ['TOTP_PARAMS'])
-resource = TotpResource.from_b32_params(params)
-
-app = FastAPI()
-app.add_api_route("/{name}", resource)
